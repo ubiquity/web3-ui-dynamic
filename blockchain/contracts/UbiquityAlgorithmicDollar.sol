731b10c280678de33326dc6065d7edb8c08fed90
Benjamin <benjamin.mateo@protonmail.com>
2021-05-08 17:49:07 +0200

    submodule

--- blockchain/contracts/UbiquityAlgorithmicDollar.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.7.0;
-
-import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
-
-contract UbiquityAlgorithmicDollar is ERC20PresetMinterPauser {
-    // solhint-disable-next-line no-empty-blocks
-    constructor() ERC20PresetMinterPauser("UbiquityAlgorithmicDollar", "uAD") {}
-}
