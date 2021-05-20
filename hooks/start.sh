0a2ace9677f7b1fbeb4dd73f61f90c1ad801e89c
Benjamin <benjamin.mateo@protonmail.com>
2021-05-20 18:22:41 +0200

    bf

--- hooks/start.sh
+++ hooks/start.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
-if [ -f ./contracts/.env ]
+if ! command -v jq &> /dev/null
 then
-  echo "DDDDDYOKL"
-  export $(cat ./contracts/.env | sed 's/#.*//g' | xargs)
+    echo "jq could not be found. Please install it."
+    exit
 fi
 
+
+if [ -f ./contracts/.env ]
+then
+  export $(cat ./contracts/.env | sed 's/#.*//g' | xargs)
+fi
+rm -f ./frontend/src/uad-contracts-deployment.json
 cd ./contracts || echo "ERROR: ./contracts/ doesn't exist?"
+
 yarn && yarn compile
 kill $(lsof -t -i:8545) || true
-echo "YOKL"
-echo $ALCHEMY_API_KEY
-echo "YOKL"
-echo $(pwd)
-yarn hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/$ALCHEMY_API_KEY --fork-block-number 12150000 --show-accounts --export-all ../frontend/src/uad-contracts-deployment.json > ../local.node.log 2>&1 &
+yarn hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/$ALCHEMY_API_KEY --fork-block-number 12150000 --show-accounts --export-all tmp-uad-contracts-deployment.json > ../local.node.log 2>&1 &
 sleep 10
 while : ; do
-    [[ -f "../frontend/src/uad-contracts-deployment.json" ]] && break
+    [[ -f "tmp-uad-contracts-deployment.json" ]] && break
     echo "Pausing until uad-contracts-deployment.json exists."
     sleep 5
 done
-
-# yarn hardhat --network localhost run scripts/deploy-entry.ts   # we need a standardized way to hit the deployment scripts before this works seamlessly
-# yarn hardhat --network localhost run deploy/*.ts & # needs to be daemonized to be able to automatically continue opening the UI
-# yarn hardhat --network localhost run deploy/00*.ts
-# for f in "$my_dir"*."$ext"; do echo $f; done
-# for deployScript in deploy/*.ts; do
-#     yarn hardhat --network localhost run "$deployScript"
-#  done
-
-#  yarn hardhat deploy --export ../frontend/src/uad-contracts-deployment.json
+jq '.[].localhost' tmp-uad-contracts-deployment.json > ../frontend/src/uad-contracts-deployment.json 
+rm -f tmp-uad-contracts-deployment.json
