9da6d310269d7b6ff2010dc3286149918770b648
アレクサンダー.eth <git@.com>
2021-05-23 23:15:14 -0500

    unique keys for inputs

--- frontend/src/components/WriteContract/listInputItem.tsx
+++ frontend/src/components/WriteContract/listInputItem.tsx
@@ -1,15 +1,22 @@
 import { sentenceCase } from "change-case";
-import React from "react";
 import { titleCase } from "title-case";
+import sha256 from "sha256";
+export function listInputItem(
+	param: { name: any; internalType: string },
+	name: string
+) {
+	const random=Math.random().toString();
+	const hashed=sha256(random);
+	const key = `${name}_${param.name}_${param.internalType}_${hashed}`;
+	console.log({ key });
 
-export function listInputItem(param: { name: any; internalType: string }) {
 	return (
-		<li key={param.name}>
+		<li key={key}>
 			<input
 				onMouseDown={(e) => e.stopPropagation()}
 				required
 				// onClick={stopBubbling}
-				key={`${param.name}_${param.internalType}_`}
+				// key={key}
 				data-type={param.internalType}
 				type={param.internalType.includes(`int`) ? "number" : "text"}
 				placeholder={titleCase(sentenceCase(param.name))}
